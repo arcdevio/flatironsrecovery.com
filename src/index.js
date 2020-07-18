0800159fa73dee5c918d4c17aea86a7a28d65579
Jon Burns <jonburns10@gmail.com>
2020-07-17 17:16:24 -0700

    color and font examples

---
+++ src/index.js
@@ -0,0 +1,7 @@
+(function() { 'use strict';
+    var font = document.createElement('link');
+    font.async = true;
+    font.rel = 'stylesheet';
+    font.href = 'https://fonts.googleapis.com/css?family=Montserrat:400,600,700';
+    document.head.appendChild(font);
+}());
