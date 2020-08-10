3910e74a5a89d05bbea414b4b12effc423b58db0
Alexander Elias <alex.steven.elias@gmail.com>
2020-08-10 11:58:27 -0700

    home page updates

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,23 @@
-(function() { 'use strict';
+
+(function(){'use strict';
+
     var font = document.createElement('link');
     font.async = true;
     font.rel = 'stylesheet';
     font.href = 'https://fonts.googleapis.com/css?family=Raleway:300,500,800';
     document.head.appendChild(font);
+
+}());
+
+(function(){'use strict';
+
+    var triggers = document.querySelectorAll('.icon');
+    var faqs = Array.prototype.slice.call(triggers);
+
+    faqs.forEach(function (faq) {
+        faq.addEventListener('click', function (e) {
+            faq.parentNode.parentNode.nextElementSibling.classList.toggle('active');
+        });
+    });
+
 }());
