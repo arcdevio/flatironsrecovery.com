87a5ce2b5f5f49bd436983b8652b2c5e00d9f007
Jon Burns <jonburns10@gmail.com>
2020-11-05 13:44:36 -0800

    all posts in array gh-27

---
+++ src/blog/data.js
@@ -0,0 +1,50 @@
+var articles = [
+    {
+        title: 'Exercise in Early Addiction Recovery Treatment',
+        author: 'Rachael Uris, MA',
+        categories: ['Mental Health'],
+        image: '/blog/images/exercise1-thumb.jpg',
+        path: '/blog/exercise-in-early-addiction-recovery-treatment',
+        description: 'Regular exercise is an essential part of holistic recovery from addiction. Not only does exercise support physical health in countless ways, it also greatly benefits mental and emotional wellbeing.',
+    },
+    {
+        title: 'A Mindfulness Exercise on Gratitude for Thanksgiving',
+        author: 'Rachael Uris, MA',
+        categories: ['Mental Health'],
+        image: '/blog/images/thanksgiving1-thumb.jpg',
+        path: '/blog/a-mindfulness-exercise-on-gratitude-for-thanksgiving',
+        description: 'A mindfulness exercise on gratitude. Consider trying this practice by reading it aloud as you sit down to your Thanksgiving dinner.',
+    },
+    {
+        title: 'EMDR in Early Addiction Recovery Treatment',
+        author: 'Rachael Uris, MA',
+        categories: ['Mental Health'],
+        image: '/blog/images/EMDR1-thumb.jpg',
+        path: '/blog/EMDR-in-early-addiction-recovery-treatment',
+        description: 'EMDR (Eye Movement Desensitization and Reprocessing) is a type of therapy that can be extremely helpful in healing from addiction, trauma, and other life stresses.',
+    },
+    {
+        title: 'Self Care Plan for Early Addiction Recovery',
+        author: 'Rachael Uris, MA',
+        categories: ['Mental Health'],
+        image: '/blog/images/selfcare-thumb.jpg',
+        path: '/blog/self-care-plan-for-early-addiction-recovery',
+        description: 'Setting the intention to practice self-love and self-care instead of self-neglect or self-harm is a powerful way to build a foundation from which we can heal and thrive.',
+    },
+    {
+        title: 'What is Mindful Eating and how can it help support  addiction recovery?',
+        author: 'Rachael Uris, MA',
+        categories: ['Mental Health'],
+        image: '/blog/images/mindfulEating1.jpg',
+        path: '/blog/what-is-mindful-eating-and-how-can-it-help-support-addiction-recovery',
+        description: 'Mindful eating is a way of having a relationship with food. A holistic approach to addiction recovery includes developing this mindful relationship to food.',
+    },
+    {
+        title: 'Mindfulness Exercises to Support Early Addiction Recovery',
+        author: 'Rachael Uris, MA',
+        categories: ['Mental Health'],
+        image: '/blog/images/mindfulness-thumb.jpg',
+        path: '/blog/mindfulness-exercises-to-support-early-addiction-recovery',
+        description: 'Mindfulness, or the practice of noticing one’s experience without judgement, has been shown to be highly effective with managing anxiety, panic, depression, anger, and drug/alcohol cravings in early recovery.',
+    }
+];
