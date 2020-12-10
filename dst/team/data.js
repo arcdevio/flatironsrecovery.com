19578df8ee05282b065d7be8b4a19e80f2d688c2
Jon Burns <jonburns10@gmail.com>
2020-12-10 15:13:59 -0800

    build

---
+++ dst/team/data.js
@@ -0,0 +1,59 @@
+window.Global = window.Global || {};
+
+window.Global.staff = [
+    {
+        image: "./team/images/mark_oberg.jpg",
+        name: "Mark Oberg LPC, LAC",
+        title: "Executive Director/Clinical Director",
+        descriptions: ['Mark received his undergraduate degree at the University of Minnesota-Duluth, master’s degree from Naropa University, in Boulder, Colorado, and brings 10+ years of clinical experience applying mindfulness-based interventions to those suffering from substance use and dual-diagnosis disorders.', 'Mark’s mindfulness-based perspective on treatment for substance use disorders is informed by the many Buddhist meditation retreats that he has participated in over the years, as well as by his daily meditation practice. Working in this way with his own emotional experience has allowed Mark more ability to look closely at himself in a kind, non-judgmental way to find the opportunity for growth in his pain, a practice he brings to his work with clients.', 'Over the course of his career working with clients to recover in residential, outpatient, and community mental health settings, Mark has seen first-hand the benefits that consistent application of mindfulness-based practices can have on people’s ability to recover. These include a greater ability to tolerate and cope in healthier ways with cravings, an increased capacity to accept oneself free from guilt and shame, as well as greater emotional and relational flexibility.', 'Mark is also trained in eye-movement and desensitization reprogramming (EMDR), DBT, and Motivational Interviewing facets of which he incorporates in various ways during both individual and group treatment.', 'In his free time, Mark enjoys spending time with his wife, son, and daughter exploring nature through hiking, camping, and paddle boarding all over the beautiful state of Colorado.  Mark also enjoys watching the Minnesota Vikings and Twins, and listening to music, in whatever free-time he has leftover.']
+    },
+    {
+        image: "./team/images/mattneptune.jpg",
+        name: "Matthew Neptune",
+        title: "Director of Operations",
+        descriptions: ['Matthew Neptune began his journey in the field of addiction and recovery in 2016 where he was a trusted House Manager at a sober living facility in Aurora, CO.', 'In 2018, Matt had the opportunity to be a core team member at a new treatment center, where he quickly rose through the ranks, eventually becoming the facility’s Director of Residential Housing.During this time, Matt was able to not only oversee the day to to day operations, but also maintain his connection with the clients.', 'In 2018, Matt opened up his own sober living home in order to provide the same opportunity that was afforded to him on his own journey in recovery.In addition to his role with Flatirons Recovery, and in operating his own sober living, Matt sits on the Board of Colorado Association of Recovery Residence(CARR).', 'Matt is thrilled to be part of such a great team at Flatirons Recovery and is looking forward to a bright future for him, his teammates, and most importantly, the clients.']
+    },
+    {
+        image: "./team/images/Sarah-Levin.jpg",
+        name: "Sarah Levin, LPC",
+        title: "Lead Therapist",
+        descriptions: ['Sarah Levin obtained her degree in mental health counseling from Naropa University in 2016. Prior to that, she spent nearly a decade studying and performing research in the fields of neuroscience and medicine. With this foundation, Sarah is passionate about educating clients about their minds. In doing so, she hopes to help them discover why they do the things they do – and how they can train themselves to do things differently.', 'In her work at Flatirons Recovery, Sarah’s approach is informed by her comprehensive understanding of the factors that contribute to the development of addiction, as well as the factors that sustain it.She strives to help clients recognize the foundation of health they have to build on; cultivate curiosity about what really matters to them; and begin to overcome the shame and guilt that perpetuates their addiction.Having recovered from a serious eating disorder that spanned many years of her life, she believes that no client is ever “too far gone” to recover - no matter if this is their first or 21st stint in treatment.', 'In her spare time, Sarah has way too many hobbies and interests, but especially enjoys playing her violin; reading everything from research articles to Rumi; and creating art out of essentially any material she can find(with varying degrees of success).']
+    },
+    {
+        image: "./team/images/rachael-uris.jpg",
+        name: "Rachael Uris, MA",
+        title: "Director of Business Development",
+        descriptions: ['Rachael has been in the mental health field in Colorado for over ten years.', 'With a Master’s Degree in Contemplative Counseling Psychology from Naropa University, Rachael has worked as a bilingual psychotherapist in various settings in Boulder, including community mental health, public schools, women’s healthcare, and private practice.', 'From her clinical background, Rachael strives to bring mindfulness, trauma - informed competence, and a holistic view of healing into her work.', 'When she’s not working, Rachael loves running, dancing, hiking, gardening, and playing outside with her two children.']
+    },
+    {
+        image: "./team/images/valerie-purdie.jpg",
+        name: "Valerie Purdie",
+        title: "Director of Admissions",
+        descriptions: ['Valerie is the Director of Admissions for Flatirons Recovery.', 'As someone with personal experience of the impact of addiction on individuals and their families, it is a passion of Valerie’s to be able to provide the resources and encouragement that will allow true change and growth in a life, free from addiction.', 'Valerie has a long career record of serving those struggling with mental health, as well as addiction issues, in a variety of positions and settings, including hospitals, residential treatment, and other community- based treatment models.', '“I believe I can be a guide, ” says Valerie, “to those trying to navigate the resources available for mental health and chemical dependency services.I am excited to be a part of the phenomenal team of providers here at Flatirons Recovery and look forward to being a part of the recovery experience for as many of those in need as possible.”']
+    },
+    {
+        image: "./team/images/jaroed-sarmeinto.jpg",
+        name: "Jaroed Sarmiento",
+        title: "House Manager",
+        descriptions: ['Jaroed is a house manager at Flatirons Recovery’s structured recovery residence. He is completing his first year of working in the treatment industry, after having been through the treatment process as a client.', 'Working as a house manager at Flatirons Recovery, Jaroed is part of the recovery housing team that introduces and supports 12-step participation with clients. He also organizes attendance at 12-step meetings, day-to-day operations for clients when they are in housing, and supports them in the beginning of their recovery journey, in part by sharing his personal recovery experiences.', '“My main goal when the clients come in,” he says, “is to show them that they can live a happy and fulfilling life sober. I try to inspire hope, because I know what it feels like to be hopeless.”', 'Jaroed spends his free time with his wife, daughter, and pets. He also enjoy disc golf, music, art, and exploring new interests.', 'Jaroed prioritizes his recovery by staying active in the 12-step community and working on his personal growth one day at a time.']
+    },
+    {
+        image: "./team/images/Layne-Jeffcoat.jpg",
+        name: "Layne Jeffcoat",
+        title: "House Manager",
+        descriptions: ['Layne has been involved in the greater-Denver recovery community for over two years.','He has a passion for helping people get involved in a program of recovery','“As someone who felt hopeless about being able to change my life and stay sober,” Layne says, “I now love being able to share my experience with others who feel the same way. Watching people’s lives change is such an amazing experience and one that I treasure.”', 'Outside of work, Layne spends his free time skiing and being active in the recovery community in Denver.']
+    },
+    {
+        image: "./team/images/kelly-miller.jpg",
+        name: "Kelly Miller",
+        title: "Nutritional Therapist",
+        descriptions: ['Kelly Miller is Flatirons Recovery’s Nutritional Therapist, and is also a certified Recovery Coach and owner of The Addiction Nutritionist; a recovery focused nutrition practice.', 'Kelly helps individuals in recovery uncover the biochemical root causes of their addictions and cravings and use that information to create targeted nutrition plans designed to improve mood, reduce cravings and prevent relapse.She has 3 + years experience working with both individuals in recovery and treatments centers around the Denver Metro area.', 'After years of struggling with her own addictions and complicated health issues.', 'Kelly discovered the powerful connection between nutrition, mood and mental health.She has now made it her life\'s work to educate and counsel others on the life changing implications of a healthy and holistic lifestyle.']
+    },
+    
+    {
+        image: "./team/images/Lindsey-McCune.jpg",
+        name: "Lindsey McCune",
+        title: "Yoga",
+        descriptions: ['Lindsey feels a great enthusiasm for recovery yoga and has been teaching for a handful of years.  One of the greatest joys in her life is the privilege to share the gifts of the practice with those starting their recovery journeys - to give back what was so freely given her.  She believes that each person can find their way to what\'s true through the body and begin a new experience one moment at a time.', 'Lindsey is a CYT, certified at The River Yoga Studio in Denver which is known for their high standards of education and teaching the teacher.She is a Hendricks Institute Leadership and Transformation program graduate, a Big Leap Coach and a leadership consultant focussing on supporting individuals and organizations to create more consciousness in whatever work they wish to do in this world.']
+    },
+]
