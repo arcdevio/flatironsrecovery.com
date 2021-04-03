ba5ce9a2847742d30919e79296f860136b23aed5
Jon Burns <jonburns10@gmail.com>
2021-04-02 19:50:55 -0700

    6ways article

--- src/blog/data.js
+++ src/blog/data.js
@@ -1,5 +1,13 @@
 var articles = [
     {
+        title: '6 Ways to Enjoy a Sober Spring in Boulder',
+        author: 'Rachael Uris, MA',
+        categories: ['mental health', 'community', 'addictions', 'treatment'],
+        image: '/blog/images/6ways-thumb.jpg',
+        path: '/blog/6-ways-to-enjoy-a-sober-spring-in-boulder',
+        description: 'This couldn’t apply more to springtime in Boulder Colorado, right at the foothills of the Rocky Mountains. Blue skies, vibrant, abundant sunshine, and budding spring flowers under the feet of the snow capped mountains.',
+    },
+    {
         title: 'What is the Difference Between CBD and THC?',
         author: 'Rachael Uris, MA',
         categories: ['mental health', 'community', 'addictions', 'treatment'],
