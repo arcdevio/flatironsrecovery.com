33457b54a29b2968dff6789f31489691b7dc93fb
Jon Burns <jonburns10@gmail.com>
2021-04-06 12:52:07 -0700

    uncomment script, team

--- src/team/data.js
+++ src/team/data.js
@@ -13,12 +13,12 @@ window.Global.staff = [
         title: "Director of Operations",
         descriptions: ['Matthew Neptune began his journey in the field of addiction and recovery in 2016 where he was a trusted House Manager at a sober living facility in Aurora, CO.', 'In 2018, Matt had the opportunity to be a core team member at a new treatment center, where he quickly rose through the ranks, eventually becoming the facility’s Director of Residential Housing.During this time, Matt was able to not only oversee the day to to day operations, but also maintain his connection with the clients.', 'In 2018, Matt opened up his own sober living home in order to provide the same opportunity that was afforded to him on his own journey in recovery.In addition to his role with Flatirons Recovery, and in operating his own sober living, Matt sits on the Board of Colorado Association of Recovery Residence(CARR).', 'Matt is thrilled to be part of such a great team at Flatirons Recovery and is looking forward to a bright future for him, his teammates, and most importantly, the clients.']
     },
-    {
-        image: "./team/images/Jeska-Albuisson.jpg",
-        name: "Jeska Albuisson",
-        title: "Medical Director",
-        descriptions: ['Jeska Albuisson is the Medical Director at Flatirons Recovery.','Jeska coordinates client medical care with the clinical team, assessing each client shortly after their arrival at Flatirons Recovery, and managing their medical needs for the duration of their treatment stay.','Jeska completed her medical training at the University of Colorado Hospital (UCH), Denver, and is board certified in Internal Medicine, she completed one year of psychiatry training during her residency at UCH. She is currently a full time hospitalist and Medical Director of hospital medicine at BCH in addition to her work at Flatirons and has been working as an Internal Medicine physician for over 12 years. Over the course of her hospital work, Jeska has had the opportunity to work with many patients hospitalized with severe withdrawal or complications related to substance use, and through that work has developed a passion for advocating for and working with patients struggling with SUD. She continues to be inspired by witnessing her patients taking on the challenge of sobriety, and having the privilege to both understand and to be a part of their story of recovery.  Jeska has worked outside of the hospital in the addiction recovery field for over 5 years and is in the process of completing her addiction medicine board certification.', 'Outside of work, Jeska enjoys spending time with her family in the mountain home she shares with her husband, 3 sons, 3 dogs and 2 cats—being a mother is the greatest joy of her life. Jeska also loves biking and frequently will be seen biking to and from work. She is originally from New York but has spent the last 20 years in Colorado and has come to love the mountain sunshine.']
-    },
+    // {
+    //     image: "",
+    //     name: "Dr. Brian K. Wise, MD, MPH, ABIHM, ABoIM",
+    //     title: "Medical Director",
+    //     descriptions: ['Dr.Wise is the Medical Director at Flatirons Recovery.He holds board certifications in Child, Adolescent, and Adult Psychiatry and is board certified by the American Board of Integrative and Holistic Medicine.He currently practices psychiatry and addiction medicine at his private practice, Wise Psychiatry, and provides psychiatric care to children and adolescents with complex medical issues at National Jewish Hospital and Lighthouse Complex Care.', 'Dr.Wise knows first-hand the struggles of being sick, as well as the hope of being healthy.He spent the summers of his youth at a major academic hospital, where physicians thought outside the box to find innovative solutions to his severe medical problems.He owes the life he has today to these gifted physicians and crafted his patient - oriented treatment philosophy after them.', 'He has a love of medical knowledge and is continuously furthering his education in a number of specialty areas, including Functional Medicine, Integrative Medicine, and the Re - Code Protocol for cognitive decline.']
+    // },
     {
         image: "./team/images/Sarah-Levin.jpg",
         name: "Sarah Levin, LPC",
@@ -31,35 +31,28 @@ window.Global.staff = [
         title: "Director of Business Development",
         descriptions: ['Rachael has been in the mental health field in Colorado for over ten years.', 'With a Master’s Degree in Contemplative Counseling Psychology from Naropa University, Rachael has worked as a bilingual psychotherapist in various settings in Boulder, including community mental health, public schools, women’s healthcare, and private practice.', 'From her clinical background, Rachael strives to bring mindfulness, trauma - informed competence, and a holistic view of healing into her work.', 'When she’s not working, Rachael loves running, dancing, hiking, gardening, and playing outside with her two children.']
     },
-    // {
-    //     image: "./team/images/valerie-purdie.jpg",
-    //     name: "Valerie Purdie",
-    //     title: "Director of Admissions",
-    //     descriptions: ['Valerie is the Director of Admissions for Flatirons Recovery.', 'As someone with personal experience of the impact of addiction on individuals and their families, it is a passion of Valerie’s to be able to provide the resources and encouragement that will allow true change and growth in a life, free from addiction.', 'Valerie has a long career record of serving those struggling with mental health, as well as addiction issues, in a variety of positions and settings, including hospitals, residential treatment, and other community- based treatment models.', '“I believe I can be a guide, ” says Valerie, “to those trying to navigate the resources available for mental health and chemical dependency services.I am excited to be a part of the phenomenal team of providers here at Flatirons Recovery and look forward to being a part of the recovery experience for as many of those in need as possible.”']
-    // },
     {
         image: "./team/images/jaroed-sarmeinto.jpg",
         name: "Jaroed Sarmiento",
         title: "House Manager",
         descriptions: ['Jaroed is a house manager at Flatirons Recovery’s structured recovery residence. He is completing his first year of working in the treatment industry, after having been through the treatment process as a client.', 'Working as a house manager at Flatirons Recovery, Jaroed is part of the recovery housing team that introduces and supports 12-step participation with clients. He also organizes attendance at 12-step meetings, day-to-day operations for clients when they are in housing, and supports them in the beginning of their recovery journey, in part by sharing his personal recovery experiences.', '“My main goal when the clients come in,” he says, “is to show them that they can live a happy and fulfilling life sober. I try to inspire hope, because I know what it feels like to be hopeless.”', 'Jaroed spends his free time with his wife, daughter, and pets. He also enjoy disc golf, music, art, and exploring new interests.', 'Jaroed prioritizes his recovery by staying active in the 12-step community and working on his personal growth one day at a time.']
     },
-    // {
-    //     image: "./team/images/Layne-Jeffcoat.jpg",
-    //     name: "Layne Jeffcoat",
-    //     title: "House Manager",
-    //     descriptions: ['Layne has been involved in the greater-Denver recovery community for over two years.','He has a passion for helping people get involved in a program of recovery','“As someone who felt hopeless about being able to change my life and stay sober,” Layne says, “I now love being able to share my experience with others who feel the same way. Watching people’s lives change is such an amazing experience and one that I treasure.”', 'Outside of work, Layne spends his free time skiing and being active in the recovery community in Denver.']
-    // },
     {
         image: "./team/images/kelly-miller.jpg",
         name: "Kelly Miller",
         title: "Nutritional Therapist",
         descriptions: ['Kelly Miller is Flatirons Recovery’s Nutritional Therapist, and is also a certified Recovery Coach and owner of The Addiction Nutritionist; a recovery focused nutrition practice.', 'Kelly helps individuals in recovery uncover the biochemical root causes of their addictions and cravings and use that information to create targeted nutrition plans designed to improve mood, reduce cravings and prevent relapse.She has 3 + years experience working with both individuals in recovery and treatments centers around the Denver Metro area.', 'After years of struggling with her own addictions and complicated health issues.', 'Kelly discovered the powerful connection between nutrition, mood and mental health.She has now made it her life\'s work to educate and counsel others on the life changing implications of a healthy and holistic lifestyle.']
     },
-
     {
         image: "./team/images/Lindsey-McCune.jpg",
         name: "Lindsey McCune",
         title: "Yoga",
         descriptions: ['Lindsey feels a great enthusiasm for recovery yoga and has been teaching for a handful of years.  One of the greatest joys in her life is the privilege to share the gifts of the practice with those starting their recovery journeys - to give back what was so freely given her.  She believes that each person can find their way to what\'s true through the body and begin a new experience one moment at a time.', 'Lindsey is a CYT, certified at The River Yoga Studio in Denver which is known for their high standards of education and teaching the teacher.She is a Hendricks Institute Leadership and Transformation program graduate, a Big Leap Coach and a leadership consultant focussing on supporting individuals and organizations to create more consciousness in whatever work they wish to do in this world.']
     },
+    // {
+    //     image: "",
+    //     name: "Kate M. Ziesenheim, PA-C",
+    //     title: "Physician Assistant",
+    //     descriptions: ['Kate Ziesenheim is the psychiatric Physician Assistant at Flatirons Recovery.She coordinates client medical care with the clinical team, assessing each client shortly after their arrival at Flatirons Recovery, and managing their medical needs for the duration of their treatment stay.','Kate completed her medical training in Physician Assistant Studies at Pacific University, in Portland, Oregon.She currently works at Wise Psychiatry, practicing outpatient psychiatry and addiction medicine, in addition to her work at Flatirons.She has an integrative and holistic and approach, which incorporates the whole body, lifestyle, and root causes of symptoms.Prior to becoming a PA, she had over 10 years of experience in the mental health and addiction field doing program management, crisis intervention, case management, intake coordination, supportive counseling, and homeless outreach.','Over the course of her career, Kate has developed a passion for supporting individuals with substance use issues on their journey to recovery.Her passion stems from personally experiencing the joys of recovery and the knowledge that her clients can experience this joy too.','Outside of work, Kate loves getting into the mountains to ski, mountain bike, trail run, and backpack.She is also an avid practitioner of yoga, meditation, and mindfulness and has been studying Buddhism since returning from a three month trip to Nepal and Southeast Asia.',]
+    // },
 ]
