57395da18aa381c41dcd9d0fbe6c1a5bec789d61
Jon Burns <jonburns10@gmail.com>
2021-05-14 13:51:22 -0700

    dynamic recent blog #47

--- src/blog/data.js
+++ src/blog/data.js
@@ -6,6 +6,7 @@ var articles = [
         image: '/blog/images/sobeliving-thumb.jpg',
         path: '/blog/what-is-structured-sober-living-and-how-is-it-beneficial',
         description: 'Sober living facilities, also known as recovery residences, can be an excellent accompaniment to drug and alcohol rehab, both during and after treatment.',
+        categories: [  ]
     },
     {
         title: 'Should I Tell my Employer About my Addiction?',
@@ -14,6 +15,7 @@ var articles = [
         image: '/blog/images/employer-thumb.jpg',
         path: '/blog/should-i-tell-my-employer-about-my-addiction',
         description: 'While there is a wide misconception that drug and alcohol addiction primarily affects those who are jobless and/or homeless, the truth is that over 70% of those who have a substance use disorder are employed.',
+        categories: [  ]
     },
     {
         title: 'Can you get Addicted to Benzodiazepines if you Take Them as Prescribed?',
@@ -22,6 +24,7 @@ var articles = [
         image: '/blog/images/benzos-thumb.jpg',
         path: '/blog/can-you-get-addicted-to-benzodiazepines-if-you-take-them-as-prescribed',
         description: 'Benzodiazepines are a group of prescription drugs that are used to treat anxiety, PTSD, panic disorders, and insomnia, among other things.',
+        categories: [ 'prescriptions' ]
     },
     {
         title: 'How to Start a Basic Meditation Practice',
@@ -30,6 +33,7 @@ var articles = [
         image: '/blog/images/meditation-thumb.jpg',
         path: '/blog/how-to-start-a-basic-meditation-practice',
         description: 'The benefits of sitting meditation and other mindfulness practices have been scientifically proven to help dramatically with a wide array of mental health issues, including drug and alcohol addiction.',
+        categories: [ 'mindfulness' ]
     },
     {
         title: 'How to get a family member into rehab',
@@ -38,6 +42,7 @@ var articles = [
         image: '/blog/images/howtogetafamily-thumb.jpg',
         path: '/blog/how-to-get-a-family-member-into-rehab',
         description: 'Watching someone you love slip into the grips of drug or alcohol addiction can be unbearably painful.',
+        categories: [  ]
     },
     {
         title: 'What are the Alternatives to AA (Alcoholics Anonymous)?',
@@ -46,6 +51,7 @@ var articles = [
         image: '/blog/images/alternativestoaa-thumb.jpg',
         path: '/blog/what-are-the-alternatives-to-aa',
         description: 'Alcoholics Anonymous (AA), Narcotics Anonymous (NA), and other Twelve Step Programing can be a great way to maintain sobriety, connect with other people in recovery from substance abuse, and continue to emotionally and spiritually grow in life after addiction.',
+        categories: [ 'alcohol' ]
     },
     {
         title: 'What is DBT, and is it an Effective Treatment for Addiction?',
@@ -54,6 +60,7 @@ var articles = [
         image: '/blog/images/dbt-thumb.jpg',
         path: '/blog/dbt-is-it-an-effective-treatment-for-addiction',
         description: 'Dialectical Behavior Therapy (DBT) is a highly-effective an evidence-based treatment modality that is used to help treat a wide range of mental health and substance abuse disorders.',
+        categories: [  ]
     },
     {
         title: 'The Importance of Being of Service in Addiction Recovery',
@@ -62,6 +69,7 @@ var articles = [
         image: '/blog/images/beingofservice-thumb.jpg',
         path: '/blog/the-importance-of-being-of-service-in-addiction-recovery',
         description: 'An important part of a truly holistic recovery from addiction is being of service to others.',
+        categories: [  ]
     },
     {
         title: 'Parenting After Rehab: How to Rebuild Your Connection to Your Children in Recovery',
@@ -70,6 +78,7 @@ var articles = [
         image: '/blog/images/parentingafterrehab-thumb.jpg',
         path: '/blog/how-to-rebuild-your-connection-to-your-children-in-recovery',
         description: 'As you go through addiction treatment, the ways in which your addiction has affected your loved ones, and especially your children, may begin to unsurface.',
+        categories: [  ]
     },
     {
         title: 'How Flatirons Recovery’s Model is Different from Traditional Residential Care',
@@ -78,6 +87,7 @@ var articles = [
         image: '/blog/images/howflatironsrecoverysmodel-thumb.jpg',
         path: '/blog/how-our-model-is-different',
         description: 'Understanding the world of rehab and addiction treatment centers can sometimes be difficult.',
+        categories: [  ]
     },
     {
         title: '6 Ways to Enjoy a Sober Spring in Boulder',
@@ -86,6 +96,7 @@ var articles = [
         image: '/blog/images/6ways-thumb.jpg',
         path: '/blog/6-ways-to-enjoy-a-sober-spring-in-boulder',
         description: 'You know what they say, "It’s a beautiful day to be sober!" This couldn’t apply more to springtime in Boulder Colorado, right at the foothills of the Rocky Mountains.',
+        categories: [  ]
     },
     {
         title: 'What is the Difference Between CBD and THC?',
@@ -94,6 +105,7 @@ var articles = [
         image: '/blog/images/cbd-thumb.jpg',
         path: '/blog/what-is-the-difference-between-cbd-and-thc',
         description: 'CBD, or Cannabidiol, is the second most prevalent compound found in cannabis. It can be derived from either hemp or marijuana, though is most often forced from hemp.',
+        categories: [ 'marijuana' ]
     },
     {
         title: 'Can Yoga Help with Overcoming Addiction?',
@@ -102,6 +114,7 @@ var articles = [
         image: '/blog/images/yoga-thumb.jpg',
         path: '/blog/can-yoga-help-with-overcoming-addiction',
         description: 'At Flatirons Recovery, we integrate yoga specifically tailored towards early addiction recovery into our programming because we know how extraordinarily beneficial it is to our clients.',
+        categories: [  ]
     },
     {
         title: 'What is Holistic Treatment for Methamphetamine Addiction?',
@@ -110,6 +123,7 @@ var articles = [
         image: '/blog/images/holisticmethtreatment-thumb.jpg',
         path: '/blog/what-is-holistic-treatment-for-methamphetamine-addiction',
         description: 'We consider methamphetamine addiction to be a coping mechanism for deeper issues in a person’s life, such as underlying depression, anxiety, stress, or trauma.',
+        categories: [ 'methamphetamine' ]
     },
     {
         title: 'Recognizing the Signs of High-Functioning Alcoholism',
@@ -118,6 +132,7 @@ var articles = [
         image: '/blog/images/highfunctioningalcoholism-rename.jpg',
         path: '/blog/recognizing-the-signs-of-high-functioning-alcoholism',
         description: 'Some may believe that to truly be an alcoholic, these consequences must be so severe that the person’s life is completely falling apart.',
+        categories: ['alcohol' ]
     },
     {
         title: 'How Mindfulness and Meditation Help with Drug and Alcohol Cravings',
@@ -126,6 +141,7 @@ var articles = [
         image: '/blog/images/mindfulness2-thumb.jpg',
         path: '/blog/mindfulness-meditation-helps-with-cravings',
         description: 'Mindfulness is a tool that has been scientifically proven to help lessen a wide array of mental health symptoms common in early addiction recovery, including cravings to use drugs or alcohol.',
+        categories: ['alcohol', 'mindfulness' ]
     },
     {
         title: 'Why are Healthy Boundaries so Important in Addiction Recovery?',
@@ -134,6 +150,7 @@ var articles = [
         image: '/blog/images/healthyboundaries-thumb.jpg',
         path: '/blog/why-are-healthy-boundaries-so-important-in-addiction-recovery',
         description: 'Learning to set and maintain healthy boundaries is a common part of early addiction recovery, and a key in maintaining a healthy, thriving long-term sober life.',
+        categories: [  ]
     },
     {
         title: 'Can I get Addicted to Marijuana?',
@@ -142,6 +159,7 @@ var articles = [
         image: '/blog/images/marijuana-thumb.jpg',
         path: '/blog/can-i-get-addicted-to-marijuana',
         description: 'Unfortunately, 30% of those who use marijuana have a form of what is called Marijuana Use Disorder, the most severe cases of which have developed a serious addiction to the drug.',
+        categories: [ 'marijuana' ]
     },
     {
         title: 'How Art Therapy Helps to Heal the Wounds of Addiction',
@@ -150,6 +168,7 @@ var articles = [
         image: '/blog/images/artterapy2-thumb.jpg',
         path: '/blog/how-art-therapy-helps-to-heal-the-wounds-of-addiction',
         description: 'When an addiction treatment center includes art therapy in their recovery curriculum, it gives clients the opportunity to process whatever feelings may be under the surface but inaccessible to the logical brain.',
+        categories: [  ]
     },
     {
         title: 'Cocaine and Anxiety: Can cocaine cause an anxiety disorder?',
@@ -158,6 +177,7 @@ var articles = [
         image: '/blog/images/cocaineandanxiety-thumb.jpg',
         path: '/blog/can-cocaine-cause-anxiety-disorder',
         description: 'Unfortunately, cocaine use is most likely going to make a person’s anxiety worse in the long run, and may even bring about clinical anxiety for someone who has not previously struggled with anxiety in the past.',
+        categories: [ 'cocaine' ]
     },
     {
         title: 'How Buddhist-Inspired Addiction Treatment is Different',
@@ -166,6 +186,7 @@ var articles = [
         image: '/blog/images/howbuddhism-thumb.jpg',
         path: '/blog/buddhist-inspired-addiction-treatment',
         description: 'All treatment modalities that contain elements of Buddhist practices have been scientifically proven to treat a wide array of mental health issues, including addiction to drugs and alcohol.',
+        categories: [  ]
     },
     {
         title: 'What Does Treatment for Heroin Look Like?',
@@ -174,6 +195,7 @@ var articles = [
         image: '/blog/images/heroin-thumb.jpg',
         path: '/blog/what-does-treatment-for-heroin-look-like',
         description: 'Heroin is a narcotic in the opiate family that originates from morphine. Heroin is an illicit drug that is sold in a white, brown, or black powder.',
+        categories: [ 'heroin' ]
     },
     {
         title: 'Benefits of Traveling for Treatment',
@@ -182,6 +204,7 @@ var articles = [
         image: '/blog/images/travelingfortreatment-thumb.jpg',
         path: '/blog/benefits-of-traveling-for-treatment',
         description: 'Addiction to drugs or alcohol develops in, and continues to be reinforced by our environment, or the “systems” in which we find ourselves, such as our family, friends, or community.',
+        categories: [  ]
     },
     {
         title: 'The Link Between Eating Disorders and Drug or Alcohol Addiction',
@@ -190,6 +213,7 @@ var articles = [
         image: '/blog/images/eatingdisorder-thumb.jpg',
         path: '/blog/eating-disorders-and-drug-or-alcohol-addiction',
         description: 'Contrary to popular belief, eating disorders do not only affect the Western world, and they don’t only affect females either; they are equal opportunity conditions.',
+        categories: ['alcohol' ]
     },
     {
         title: 'What is Sex Addiction, and is it Similar to Substance Addiction?',
@@ -198,6 +222,7 @@ var articles = [
         image: '/blog/images/sexaddiction-thumb.jpg',
         path: '/blog/what-is-sex-addiction',
         description: 'Sex addiction happens when sexual behaviors negatively impact our daily functioning, and we find ourselves unable to change our behaviors despite their negative consequences.',
+        categories: [  ]
     },
     {
         title: 'We Aren’t Here to Fix you Because You’re not Broken',
@@ -206,6 +231,7 @@ var articles = [
         image: '/blog/images/wearentheartofixyou-thumb.jpg',
         path: '/blog/we-arent-here-to-fix-you-because-youre-not-broken',
         description: 'Our Foundation of Basic Goodness. Basic Goodness is the concept that we are—innately and forever—good at our core.',
+        categories: [  ]
     },
     {
         title: 'COVID-19, Loneliness, and Addiction',
@@ -214,6 +240,7 @@ var articles = [
         image: '/blog/images/man-mask-thumb.jpg',
         path: '/blog/COVID-19-loneliness-and-addiction',
         description: 'Over the course of the COVID-19 pandemic, our country has witnessed a devastating spike in substance abuse, addiction, and drug and alcohol related deaths.',
+        categories: [  ]
     },
     {
         title: 'What are the Effects of Alcohol Addiction?',
@@ -222,6 +249,7 @@ var articles = [
         image: '/blog/images/alcohol-addiction-thumb.jpg',
         path: '/blog/what-are-the-effects-of-alcohol-addiction',
         description: 'Alcohol affects the brain on a number of levels, learn about the effects of alcoholism and the signs of alcohol addiction.',
+        categories: ['alcohol' ]
     },
     {
         title: 'Five New Years Resolutions for Early Recovery',
@@ -230,6 +258,7 @@ var articles = [
         image: '/blog/images/fivenewyearsresolutions-thumb.jpg',
         path: '/blog/five-new-years-resolutions-for-early-recovery',
         description: 'Yes, we want to grow. Of course we do! Why else would we be on the recovery path if not to better ourselves and our lives?',
+        categories: [  ]
     },
     {
         title: 'Mental Illness and Meditation',
@@ -238,6 +267,7 @@ var articles = [
         image: '/blog/images/mentalillnessandmeditation-thumb.jpg',
         path: '/blog/mental-illness-and-meditation',
         description: 'The skeptical scientist in me looked for tangible things to grasp, and I couldn’t see how mindfulness had anything to do with science...',
+        categories: [  ]
     },
     {
         title: 'Aimless Wandering: a Mindfulness Practice for Early Addiction Recovery',
@@ -246,6 +276,7 @@ var articles = [
         image: '/blog/images/aimless-wandering-thumb.jpg',
         path: '/blog/aimless-wandering',
         description: 'Mindfulness has been scientifically proven to decrease substance use cravings, depression, anxiety, stress, and panic.',
+        categories: [ 'mindfulness' ]
     },
     {
         title: 'Addiction Recovery During the Holidays: How to Cope, Grieve, and Heal',
@@ -254,6 +285,7 @@ var articles = [
         image: '/blog/images/holidays-thumb.jpg',
         path: '/blog/addiction-recovery-during-the-holidays',
         description: 'For many, especially for those recovering from addiction, the holidays bring a blizzard of emotional turmoil that is hard to understand.',
+        categories: [  ]
     },
     {
         title: 'Exercise in Early Addiction Recovery Treatment',
@@ -262,6 +294,7 @@ var articles = [
         image: '/blog/images/exercise1-thumb.jpg',
         path: '/blog/exercise-in-early-addiction-recovery-treatment',
         description: 'Regular exercise is an essential part of holistic recovery from addiction. Not only does exercise support physical health in countless ways, it also greatly benefits mental and emotional wellbeing.',
+        categories: [  ]
     },
     {
         title: 'A Mindfulness Exercise on Gratitude for Thanksgiving',
@@ -270,6 +303,7 @@ var articles = [
         image: '/blog/images/thanksgiving1-thumb.jpg',
         path: '/blog/a-mindfulness-exercise-on-gratitude-for-thanksgiving',
         description: 'A mindfulness exercise on gratitude. Consider trying this practice by reading it aloud as you sit down to your Thanksgiving dinner.',
+        categories: [ 'mindfulness' ]
     },
     {
         title: 'EMDR in Early Addiction Recovery Treatment',
@@ -278,6 +312,7 @@ var articles = [
         image: '/blog/images/EMDR1-thumb.jpg',
         path: '/blog/emdr-in-early-addiction-recovery-treatment',
         description: 'EMDR (Eye Movement Desensitization and Reprocessing) is a type of therapy that can be extremely helpful in healing from addiction, trauma, and other life stresses.',
+        categories: [  ]
     },
     {
         title: 'Self Care Plan for Early Addiction Recovery',
@@ -286,6 +321,7 @@ var articles = [
         image: '/blog/images/selfcare-thumb.jpg',
         path: '/blog/self-care-plan-for-early-addiction-recovery',
         description: 'Setting the intention to practice self-love and self-care instead of self-neglect or self-harm is a powerful way to build a foundation from which we can heal and thrive.',
+        categories: [  ]
     },
     {
         title: 'What is Mindful Eating and how can it help support addiction recovery?',
@@ -294,6 +330,7 @@ var articles = [
         image: '/blog/images/mindfulEating1.jpg',
         path: '/blog/what-is-mindful-eating-and-how-can-it-help-support-addiction-recovery',
         description: 'Mindful eating is a way of having a relationship with food. A holistic approach to addiction recovery includes developing this mindful relationship to food.',
+        categories: [  ]
     },
     {
         title: 'Mindfulness Exercises to Support Early Addiction Recovery',
@@ -302,5 +339,6 @@ var articles = [
         image: '/blog/images/mindfulness-thumb.jpg',
         path: '/blog/mindfulness-exercises-to-support-early-addiction-recovery',
         description: 'Mindfulness, or the practice of noticing one’s experience without judgement, has been shown to be highly effective with managing anxiety, panic, depression, anger, and drug/alcohol cravings in early recovery.',
+        categories: [ 'mindfulness' ]
     }
 ];
